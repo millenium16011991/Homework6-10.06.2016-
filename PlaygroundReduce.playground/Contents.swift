dfad09b9737f617d2d90a7336a5df35b6a6f3280
vitaliy.manov <manu1991@yandex.ru>
2016-06-10 14:15:10 +0300

    Я добавил 4 файла с д/з: Map, Filter, Reduce, Sort(в reduce, я уверен не то, что ты бы хотел увидить, но я не знаю как иначе объединить строки:(   )

---
+++ PlaygroundReduce.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Person{
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+}
+
+var person1 = Person(name: "Jack", age: 45)
+var person2 = Person(name: "Wolly", age: 34)
+var person3 = Person(name: "Jannet", age: 25)
+var person4 = Person(name: "Steave", age: 27)
+var person5 = Person(name: "Gabriella", age: 42)
+class Car{
+    var driver: Person
+    var model: String
+    var weght: Int
+    var maxSpeed: Int
+    
+    init(driver: Person, model: String, weght: Int, maxSpeed: Int){
+        self.driver = driver
+        self.model = model
+        self .weght = weght
+        self.maxSpeed = maxSpeed
+    }
+}
+var car1 = Car(driver: person1, model: "Lexus", weght: 3000, maxSpeed: 270)
+var car2 = Car(driver: person2, model: "Audi", weght: 2300, maxSpeed: 235)
+var car3 = Car(driver: person3, model: "Ford", weght: 1700, maxSpeed: 210)
+var car4 = Car(driver: person4, model: "Lamborgini", weght: 2100, maxSpeed: 350)
+var car5 = Car(driver: person5, model: "Bugatti", weght: 2500, maxSpeed: 405)
+
+var cars = [car1, car2, car3, car4, car5]
+
+var concatination: ((String?, String?) -> String)
+
+func concatinationFunction(first: String?, second: String?) -> String {
+    guard let name = first, let model = second else{
+        return ""}
+    return name+model
+}
+concatination = concatinationFunction
+concatination(car1.driver.name, car1.model)
+concatination(car2.driver.name, car2.model)
+concatination(car3.driver.name, car3.model)
+concatination(car4.driver.name, car4.model)
+concatination(car5.driver.name, car5.model)
+
+
+
+
+
+
---
+++ PlaygroundSort.playground/Contents.swift
@@ -0,0 +1,57 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Person{
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+}
+
+var person1 = Person(name: "Jack", age: 45)
+var person2 = Person(name: "Wolly", age: 34)
+var person3 = Person(name: "Jannet", age: 25)
+var person4 = Person(name: "Steave", age: 27)
+var person5 = Person(name: "Gabriella", age: 42)
+class Car{
+    var driver: Person
+    var model: String
+    var weght: Int
+    var maxSpeed: Int
+    
+    init(driver: Person, model: String, weght: Int, maxSpeed: Int){
+        self.driver = driver
+        self.model = model
+        self .weght = weght
+        self.maxSpeed = maxSpeed
+    }
+}
+var car1 = Car(driver: person1, model: "Lexus", weght: 3000, maxSpeed: 270)
+var car2 = Car(driver: person2, model: "Audi", weght: 2300, maxSpeed: 235)
+var car3 = Car(driver: person3, model: "Ford", weght: 1700, maxSpeed: 210)
+var car4 = Car(driver: person4, model: "Lamborgini", weght: 2100, maxSpeed: 350)
+var car5 = Car(driver: person5, model: "Bugatti", weght: 2500, maxSpeed: 405)
+
+var cars = [car1, car2, car3, car4, car5]
+
+var persons = [person1, person2, person3, person4, person5]
+
+var sortOfAgeDriver1 = persons.sort {$0.age < $1.age}
+sortOfAgeDriver1
+
+var sortOfAgeDriver2 = cars.sort {$0.driver.age < $1.driver.age}
+sortOfAgeDriver2
+
+var sortOfAgeDriverBecome1 = persons.sort {$0.age > $1.age}
+sortOfAgeDriverBecome1
+
+var sortOfAgeDriverBecome2 = cars.sort {$0.driver.age > $1.driver.age}
+sortOfAgeDriverBecome2
+
+
